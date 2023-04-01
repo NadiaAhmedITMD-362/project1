d19df1f604c948a6a4ce1472b7ed5e7665a34b82
nahmed15 <112483909+nahmed15@users.noreply.github.com>
2023-04-01 01:42:32 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,14 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-});
+const form = document.getElementById('signup-form');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('email-error');
+    
+    form.addEventListener('submit', function(event) {
+      // Prevent the form from submitting
+      event.preventDefault();
+    
+      // Email validation
+      if (!emailInput.checkValidity()) {
+        emailError.textContent = 'Please enter a valid email address.';
+        emailError.style.display = 'block';
+      } else {
+        emailError.style.display = 'none';
