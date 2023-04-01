9bb07d8a304717ac2adb12976c5adf2bdf44504e
nahmed15 <112483909+nahmed15@users.noreply.github.com>
2023-04-01 01:45:48 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,14 +1,30 @@
-const form = document.getElementById('signup-form');
-    const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('email-error');
-    
-    form.addEventListener('submit', function(event) {
-      // Prevent the form from submitting
-      event.preventDefault();
-    
-      // Email validation
-      if (!emailInput.checkValidity()) {
-        emailError.textContent = 'Please enter a valid email address.';
-        emailError.style.display = 'block';
-      } else {
-        emailError.style.display = 'none';
+// get form and input fields
+const form = document.querySelector('#signup-form');
+const fullNameInput = document.querySelector('#fullName');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const phoneNumberInput = document.querySelector('#phoneNumber');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirmPassword');
+
+// get error message elements
+const emailError = document.querySelector('#email-error');
+
+// handle form submission
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (!validateEmail()) {
+    emailError.innerText = 'Invalid email address';
+  } else {
+    emailError.innerText = '';
+    alert('Form submitted successfully!');
+    form.reset();
+  }
+});
+
+// validate email format
+function validateEmail() {
+  const email = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
